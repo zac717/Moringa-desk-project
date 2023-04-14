eb015304a9206c9fdb2fb60901d83af00fdf0305
Mohamed <aminito432@gmail.com>
2023-04-14 15:22:13 +0300

    updated the specs model tag_specs.rb

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
-
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:question_tags) }
+  it { should have_many(:questions).through(:question_tags) }
+  it { should validate_presence_of(:name) }
 end
