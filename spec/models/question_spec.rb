3dcff78e41aff573007ba1c7ac44567f03c9c52e
Mohamed <aminito432@gmail.com>
2023-04-14 15:17:11 +0300

    updated the specs model question_spec.rb file

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
-
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:answers) }
+  it { should have_many(:question_tags) }
+  it { should have_many(:tags).through(:question_tags) }
+  it { should have_many(:notifications).dependent(:destroy) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
 end
