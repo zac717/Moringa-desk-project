ba469fe39d25a25bddb52679fea9a4bd86b12fd9
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-04-17 12:47:36 +0300

    Login done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   
     def create
-      user = User.find_by(email: params[:session][:email].downcase)
+      user = User.find_by(username: params[:session][:username].downcase)
       if user && user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to root_path
+        session[:user_id] = user.id
+        render json: user, status: :created
       else
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
