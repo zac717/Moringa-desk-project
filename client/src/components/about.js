af8ca6d2ef8a089b82b071a49d017ea31c3cc3a4
Mohamed <aminito432@gmail.com>
2023-04-30 11:47:29 +0300

    updated the about us file

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -7,10 +7,11 @@ const AboutUs = () => {
       <p>We are a team of dedicated professionals who are passionate about creating high-quality software solutions for our clients. Our mission is to help businesses and organizations succeed by leveraging the power of technology.</p>
       <h2>Our Team</h2>
       <ul>
-        <li>John Doe - CEO</li>
-        <li>Jane Smith - CTO</li>
-        <li>Bob Johnson - Lead Developer</li>
-        <li>Samantha Lee - Designer</li>
+        <li>Mohamed Amin - Lead Developer</li>
+        <li>Evalyne Mueke - Back End Team</li>
+        <li>Sumeya Haji - Back End Team </li>
+        <li>Isaac Atuti - Front End Team</li>
+        <li>George Karanja - Front End Team</li>
       </ul>
     </div>
   );
